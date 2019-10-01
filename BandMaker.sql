001745f83d45e3aa4177910c4a4ade5ce5c66426
Gabrielle Adriano <51218947+Ga-Bs@users.noreply.github.com>
2019-10-01 14:16:11 -0300

    teste banco

--- BandMaker.sql
+++ BandMaker.sql
@@ -11,11 +11,70 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura do banco de dados para bandmaker
+CREATE DATABASE IF NOT EXISTS `bandmaker` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `bandmaker`;
+
+-- Copiando estrutura para tabela bandmaker.evento
+CREATE TABLE IF NOT EXISTS `evento` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `data` datetime NOT NULL,
+  `preco` decimal(5,2) NOT NULL,
+  `banda` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Exportação de dados foi desmarcado.
 
+-- Copiando estrutura para tabela bandmaker.perfil_banda
+CREATE TABLE IF NOT EXISTS `perfil_banda` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `cidade` varchar(30) NOT NULL,
+  `estado` varchar(30) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `influencias` varchar(100) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `imagem` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela bandmaker.perfil_banda_has_evento
+CREATE TABLE IF NOT EXISTS `perfil_banda_has_evento` (
+  `perfil_banda_has_evento_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `perfil_banda_id` int(11) unsigned NOT NULL,
+  `evento_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`perfil_banda_has_evento_id`),
+  KEY `perfildabandafkk_idx` (`perfil_banda_id`),
+  KEY `eventosfkk_idx` (`evento_id`),
+  CONSTRAINT `eventosfkk` FOREIGN KEY (`evento_id`) REFERENCES `evento` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `perfildabandafkk` FOREIGN KEY (`perfil_banda_id`) REFERENCES `perfil_banda` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela bandmaker.perfil_normal
+CREATE TABLE IF NOT EXISTS `perfil_normal` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nome_usuário` varchar(24) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  `instrumento` varchar(30) NOT NULL,
+  `tempo` varchar(30) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `influencia` varchar(255) DEFAULT NULL,
+  `banda_id` int(11) unsigned DEFAULT NULL,
+  `imagem` varchar(255) DEFAULT NULL,
+  `senha` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `bandaidfk_idx` (`banda_id`),
+  CONSTRAINT `bandaidfk` FOREIGN KEY (`banda_id`) REFERENCES `perfil_banda` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- Exportação de dados foi desmarcado.
 
