244ae51337202774816e22af8d1fb5ee384fb80d
Gabriel Eduardo Cunha <gabriel_e_cunha@estudante.sc.senai.br>
2019-10-07 22:46:51 -0300

    Continua logado navegando entre as telas
    Auto explicativo :)

--- src/routes/eventosRoute.php
+++ src/routes/eventosRoute.php
@@ -7,10 +7,18 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/eventos/', function (Request $request, Response $response, array $args) use ($container) {
+    $app->get('/eventos/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/eventos/' route");
+        
+        $conexao = $container->get('pdo');
+        if (!isset($args['id'])) {
+            $resultSet = "";
+        } else {
+            $resultSet = $conexao->query('SELECT * FROM perfil_normal WHERE id = ' . $args['id'])->fetchAll();
+        }
 
+        $args['perfil_normal'] = $resultSet;
 
         // Render index view
         return $container->get('renderer')->render($response, 'eventos.phtml', $args);
