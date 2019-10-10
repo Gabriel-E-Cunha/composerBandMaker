33cb91ee035948bcea32546ec3cec4ccaf28d467
Your Name <you@example.com>
2019-10-10 14:06:49 -0300

    Implementação do Section

--- src/routes/cadastroBandaRoute.php
+++ src/routes/cadastroBandaRoute.php
@@ -7,9 +7,16 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/criarBanda/', function (Request $request, Response $response, array $args) use ($container) {
+    $app->get('/criarBanda/[{erro}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/criarBanda/' route");
+
+        if(isset($args['erro']) && $args['erro'] == "error") {
+            $args['cadastroError'] = true;
+        } else {
+            $args['cadastroError'] = false;
+        }
+
         // Render index view
         return $container->get('renderer')->render($response, 'cadastroBanda.phtml', $args);
     });
@@ -32,11 +39,11 @@ return function (App $app) {
         $resultSet = $conexao->query('SELECT * FROM perfil_banda
         WHERE email = "' . $params['email'] . '"')->fetchAll();
 
-        if ($conexao != null) {
-            return $response->withRedirect('/' . $resultSet[0]['id']);
+        if ($resultSet != null) {
+            $_SESSION['loginID'] = $resultSet[0]['id'];
+            return $response->withRedirect('/');
         } else {
-            return $response->withRedirect('/criarBanda/');
-            exit;
+            return $response->withRedirect('/criarBanda/error');
         }
 
 
