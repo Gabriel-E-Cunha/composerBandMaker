4b13ae979f3a36f95474482a98b5414a3eb8bb87
Gabriel Eduardo Cunha <gabriel_e_cunha@estudante.sc.senai.br>
2019-10-10 21:03:52 -0300

    Atualização completa do Cadastro de pessoas

--- src/routes/cadastroBandaRoute.php
+++ src/routes/cadastroBandaRoute.php
@@ -7,15 +7,11 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/criarBanda/[{erro}]', function (Request $request, Response $response, array $args) use ($container) {
+    $app->get('/criarBanda/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/criarBanda/' route");
 
-        if(isset($args['erro']) && $args['erro'] == "error") {
-            $args['cadastroError'] = true;
-        } else {
-            $args['cadastroError'] = false;
-        }
+        
 
         // Render index view
         return $container->get('renderer')->render($response, 'cadastroBanda.phtml', $args);
--- src/routes/cadastroNormalRoute.php
+++ src/routes/cadastroNormalRoute.php
@@ -11,13 +11,38 @@ return function (App $app) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/criarConta/' route");
 
-    
+
 
 
 
         // Render index view
         return $container->get('renderer')->render($response, 'cadastroNormal.phtml', $args);
+    });
 
+    $app->post('/criarConta/', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/criarConta/' route");
+
+        $conexao = $container->get('pdo');
+        $params = $request->getParsedBody();
+
+        $conexao->query('INSERT INTO perfil_pessoa (nome_usuario,nome,sobrenome,email,idade,cidade,estado,instrumento,
+        tempo,telefone,influencia,cep,rua) 
+        VALUES("' . $params['nome_usuario'] . '", "' . $params['nome'] . '", "' . $params['sobrenome'] . '",
+        "' . $params['email'] . '", "' . $params['idade'] . '", "' . $params['cidade'] . '", "' . $params['estado'] . '",
+        "' . $params['instrumento'] . '", "' . $params['tempo'] . '", "' . $params['telefone'] . '", "' . $params['influencia'] . '","'. $params['cep'] .'", "'. $params['rua'] .'")');
+
+        $resultSet = $conexao->query('SELECT * FROM perfil_pessoa
+        WHERE nome_usuario = "' . $params['nome_usuario'] . '"')->fetchAll();
+
+        $conexao->query('INSERT INTO dado_login (nome_usuario, senha, pessoa_id)
+        VALUES("' . $params['nome_usuario'] . '", "' . md5($params['senha']) . '", "' . $resultSet[0]['id'] . '")');
+
+        $_SESSION['banda'] = false;
+        $_SESSION['loginID'] = $resultSet[0]['id'];
+        return $response->withRedirect('/');
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'cadastroNormal.phtml', $args);
     });
 };
- 
