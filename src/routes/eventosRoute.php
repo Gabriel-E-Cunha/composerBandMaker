85a852c614a0b746743bedc1460dd4f67f267ef3
Your Name <you@example.com>
2019-10-16 15:11:47 -0300

    Eventos

--- src/routes/eventosRoute.php
+++ src/routes/eventosRoute.php
@@ -7,18 +7,23 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/eventos/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
+    $app->get('/eventos/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/eventos/' route");
-        
-        $conexao = $container->get('pdo');
-        if (!isset($args['id'])) {
-            $resultSet = "";
-        } else {
-            $resultSet = $conexao->query('SELECT * FROM perfil_pessoa WHERE id = ' . $args['id'])->fetchAll();
-        }
 
-        $args['perfil_pessoa'] = $resultSet;
+        $conexao = $container->get('pdo');
+        
+            if($_SESSION['banda']) {
+                $args['banda'] = true;
+                $resultSet = $conexao->query('SELECT * FROM perfil_banda WHERE id = ' . $_SESSION['loginID'])->fetchAll();
+                $args['eventos'] = $conexao->query('SELECT * FROM evento WHERE banda_id = ' . $resultSet[0]['id']);
+            } else {
+                $args['banda'] = false;
+                $resultSet = $conexao->query('SELECT * FROM perfil_pessoa WHERE id = ' . $_SESSION['loginID'])->fetchAll();
+                $args['eventos'] = $conexao->query('SELECT * FROM evento WHERE banda_id = ' . $resultSet[0]['banda_id']); 
+            }
+            $args['perfil'] = $resultSet;
+            
 
         // Render index view
         return $container->get('renderer')->render($response, 'eventos.phtml', $args);
