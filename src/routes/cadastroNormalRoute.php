2b271c0902ca1200a50ab5b11ccf2ebb2abf0bdf
Gustavo Palmeirense <gustavo02.gurtler@gmail.com>
2019-10-24 16:03:28 -0300

    Add funcionalidade de upload img

--- src/routes/cadastroNormalRoute.php
+++ src/routes/cadastroNormalRoute.php
@@ -19,11 +19,12 @@ return function (App $app) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/criarConta/' route");
 
+
+
         $conexao = $container->get('pdo');
         $params = $request->getParsedBody();
 
         $resultSet = $conexao->query('SELECT nome_usuario FROM perfil_pessoa WHERE nome_usuario = "'. $params['nome_usuario'] .'"')->fetchAll();
-
         if (
             $params['nome_usuario'] == null || $params['nome'] == null || $params['cidade'] == null || $params['cep'] == null ||
             $params['sobrenome'] == null || $params['estado'] == null || $params['email'] == null || $params['idade'] == null ||
@@ -50,6 +51,14 @@ return function (App $app) {
 
             $conexao->query('INSERT INTO dado_login (nome_usuario, senha, pessoa_id)
             VALUES("' . $params['nome_usuario'] . '", "' . md5($params['senha']) . '", "' . $resultSet[0]['id'] . '")');
+
+            //Tratamento da imagem
+            $imgName = "profile".$resultSet[0]['id']."." . explode('/',$_FILES["img"]["type"])[1];
+            $target_dir = "public/assets/profileImg/";
+            $target_file = $target_dir . $imgName;
+            move_uploaded_file($_FILES["img"]["tmp_name"], $target_file);   
+
+            $conexao->query('UPDATE perfil_pessoa SET imagem = "'.$imgName.'" WHERE id = ' . $resultSet[0]['id']);
 
             $_SESSION['banda'] = false;
             $_SESSION['loginID'] = $resultSet[0]['id'];
