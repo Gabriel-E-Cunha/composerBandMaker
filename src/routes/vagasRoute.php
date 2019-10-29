6201a7a7105b0119a4a0f495551a078c2489ebd9
Gabs <gabrielle.A.adriano@gmail.com>
2019-10-29 17:05:01 -0300

    Pedidos

--- src/routes/vagasRoute.php
+++ src/routes/vagasRoute.php
@@ -13,29 +13,56 @@ return function (App $app) {
 
         $conexao = $container->get('pdo');
 
-        $resultSet0 = $conexao->query('SELECT genero FROM perfil_banda INNER JOIN
-        vaga WHERE vaga.banda_id = perfil_banda.id')->fetchAll();
-        $args['genero'] = $resultSet0;
+        if ($_SESSION['banda'] == false) {
 
-        $resultSet00 = $conexao->query('SELECT vaga FROM vaga')->fetchAll();
-        $args['vaga'] = $resultSet00;
+            $resultSet = $conexao->query('SELECT banda_id FROM vaga INNER JOIN
+            perfil_banda WHERE vaga.banda_id = perfil_banda.id')->fetchAll();
 
-        if (isset($_GET['busca'])) {
-            $resultSet01 = $conexao->query('SELECT * FROM perfil_banda INNER JOIN
-            vaga WHERE vaga.banda_id = perfil_banda.id AND
-            perfil_banda.nome_usuario LIKE "%' . $_GET['busca'] . '%"')->fetchAll();
-            $args['resultado'] = $resultSet01;
+            if (isset($_GET['busca'])) {
+                $resultSet01 = $conexao->query('SELECT * FROM perfil_banda INNER JOIN
+                vaga WHERE vaga.banda_id = perfil_banda.id AND
+                perfil_banda.nome_usuario LIKE "%' . $_GET['busca'] . '%"')->fetchAll();
+                $args['resultado'] = $resultSet01;
+            }
+            if (isset($_GET['genero']) && $_GET['genero'] != "Gênero") {
+                $resultSet01 = $conexao->query('SELECT * FROM perfil_banda INNER JOIN
+                vaga WHERE vaga.banda_id = perfil_banda.id AND perfil_banda.genero ="' . $_GET['genero'] . '"')->fetchAll();
+                $args['resultado'] = $resultSet01;
+            }
+            if (isset($_GET['vaga']) && $_GET['vaga'] != "Vaga") {
+                $resultSet01 = $conexao->query('SELECT * FROM perfil_banda INNER JOIN
+                vaga WHERE vaga.banda_id = perfil_banda.id AND vaga.vaga = "' . $_GET['vaga'] . '"')->fetchAll();
+                $args['resultado'] = $resultSet01;
+            }
+        }else {            
+            $resultSet = $conexao->query('SELECT * FROM vaga
+            WHERE vaga.banda_id = '.$_SESSION['banda'])->fetchAll();
+            $args['resultado'] = $resultSet;
+
+            $resultSet = $conexao->query('SELECT * FROM perfil_pessoa INNER JOIN
+            pedido WHERE pedido.usuario = perfil_pessoa.id')->fetchAll();
+            $args['usuario'] = $resultSet;
+
         }
-        if (isset($_GET['genero']) && $_GET['genero'] != "Gênero") {
-            $resultSet01 = $conexao->query('SELECT * FROM perfil_banda INNER JOIN
-            vaga WHERE vaga.banda_id = perfil_banda.id AND perfil_banda.genero ="' . $_GET['genero'] . '"')->fetchAll();
-            $args['resultado'] = $resultSet01;
-        }
-        if (isset($_GET['vaga']) && $_GET['vaga'] != "Vaga") {
-            $resultSet01 = $conexao->query('SELECT * FROM perfil_banda INNER JOIN
-            vaga WHERE vaga.banda_id = perfil_banda.id AND vaga.vaga = "' . $_GET['vaga'] . '"')->fetchAll();
-            $args['resultado'] = $resultSet01;
-        }
+
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'vagas.phtml', $args);
+    });
+    $app->post('/vagas/', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/vagas/' route");
+
+        $conexao = $container->get('pdo');
+        $params = $request->getParsedBody();
+
+        if ($_SESSION['banda'] == false) {
+            $resultSet = $conexao->query('SELECT * FROM vaga
+        WHERE id = "' . $params['vaga'] . '"')->fetchAll();
+
+            $conexao->query('INSERT INTO pedido(usuario,vaga,banda_destino)
+        VALUES(' . $_SESSION['loginID'] . ',' . $resultSet[0]['id'] . ',' . $resultSet[0]['banda_id'] . ')');
+        } else { }
 
         // Render index view
         return $container->get('renderer')->render($response, 'vagas.phtml', $args);
