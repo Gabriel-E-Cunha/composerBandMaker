5ef96c2777c7febc9fa53d283f13842a155cd40f
Gabriel Eduardo Cunha <gabriel_e_cunha@estudante.sc.senai.br>
2019-10-30 22:48:21 -0300

    Sistema de aceitar e recusar pessoa
    A banda agora pode em suas notificações aceitar ou recusar uma pessoa para preencher uma vaga.

--- src/routes/notificacoesRoute.php
+++ src/routes/notificacoesRoute.php
@@ -10,25 +10,57 @@ return function (App $app) {
     $app->get('/notificacoes/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/notificacoes/' route");
-            $conexao = $container->get('pdo');
+        $conexao = $container->get('pdo');
 
-            if($_SESSION['banda']) {
-                $resultSet = $conexao->query('SELECT titulo, notificacao.descricao FROM notificacao INNER JOIN perfil_banda WHERE perfil_banda.id = '.$_SESSION['loginID'].' AND perfil_banda.id = banda_id')->fetchAll();
-            } else {
-                $resultSet = $conexao->query('SELECT titulo, descricao FROM notificacao INNER JOIN perfil_pessoa WHERE perfil_pessoa.id = '.$_SESSION['loginID'].' AND perfil_pessoa.id = pessoa_id')->fetchAll();
-            }
-            $args['notificacoes'] = $resultSet;
+        if ($_SESSION['banda']) {
+            $resultSet = $conexao->query('SELECT titulo, notificacao.descricao, pedido_id FROM notificacao INNER JOIN perfil_banda WHERE perfil_banda.id = ' . $_SESSION['loginID'] . ' AND perfil_banda.id = banda_id')->fetchAll();
+        } else {
+            $resultSet = $conexao->query('SELECT titulo, descricao, pedido_id FROM notificacao INNER JOIN perfil_pessoa WHERE perfil_pessoa.id = ' . $_SESSION['loginID'] . ' AND perfil_pessoa.id = pessoa_id')->fetchAll();
+        }
+        $args['notificacoes'] = $resultSet;
 
         // Render index view
         return $container->get('renderer')->render($response, 'notificacoes.phtml', $args);
     });
-    $app->post('/notificacoes/', function (Request $request, Response $response, array $args) use ($container) {
+
+    $app->delete('/notificacoes/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/notificacoes/' route");
+        $conexao = $container->get('pdo');
+        $params = $request->getParsedBody();
 
-        
+
 
         // Render index view
         return $container->get('renderer')->render($response, 'notificacoes.phtml', $args);
+    });
+
+    $app->post('/notificacoes/', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/notificacoes/' route");
+        $conexao = $container->get('pdo');
+        $params = $request->getParsedBody();
+
+        if ($params['botao'] == 'aceitar') {
+            $resultSet = $conexao->query('SELECT vaga_id,usuario FROM pedido WHERE id = ' . $params['pedido_id'])->fetchAll();
+            //Coloca o usuário na Banda
+            $conexao->query('UPDATE perfil_pessoa SET banda_id = ' . $_SESSION['loginID'] . ' WHERE id = ' . $resultSet[0]['usuario'])->fetchAll();
+
+            //Deleta todas as notificações e pedidos envolvidos com a vaga
+            $conexao->query('DELETE FROM notificacao WHERE pedido_id IN(SELECT id FROM pedido WHERE usuario = '.$resultSet[0]['usuario'].')')->fetchAll();
+            $conexao->query('DELETE FROM notificacao WHERE pedido_id IN(SELECT id FROM pedido WHERE vaga_id = '.$resultSet[0]['vaga_id'].')')->fetchAll();
+            $conexao->query('DELETE FROM pedido WHERE usuario = ' . $resultSet[0]['usuario'])->fetchAll();
+            $conexao->query('DELETE FROM pedido WHERE vaga_id = ' . $resultSet[0]['vaga_id'])->fetchAll();
+            //Deleta a vaga
+            $conexao->query('DELETE FROM vaga WHERE id = ' . $resultSet[0]['vaga_id'])->fetchAll();
+
+        } else if($params['botao'] == 'recusar'){
+            //Deleta a notificação e o pedido
+            $conexao->query('DELETE FROM notificacao WHERE pedido_id = ' . $params['pedido_id'])->fetchAll();
+            $conexao->query('DELETE FROM pedido WHERE id = ' . $params['pedido_id'])->fetchAll();
+        }
+
+        return $response->withRedirect('/notificacoes/');
+
     });
 };
